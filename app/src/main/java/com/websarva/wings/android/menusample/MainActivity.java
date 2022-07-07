30ac51b5503c61bca127b99a54f7263aeedfc602
MatsuokaKota <y200186@mail.ryukoku.ac.jp>
2022-07-07 12:01:06 +0900

    konnnitiha

--- app/src/main/java/com/websarva/wings/android/menusample/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/menusample/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -43,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         _lvMenu.setAdapter(adapter);
         //リストタップのリスナクラスの登録
         _lvMenu.setOnItemClickListener(new ListItemClickListener());
+        registerForContextMenu(_lvMenu);
     }
 
     private List<Map<String, Object>> createTeishokuList(){
@@ -87,17 +90,7 @@ public class MainActivity extends AppCompatActivity {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id){
             //タップされた行のデータを取得．SimpleAdapterでは1行分のデータはMap型!
             Map<String, Object> item = (Map<String, Object>) parent.getItemAtPosition(position);
-            //定食名と金額を取得
-            String menuName = (String) item.get("name");
-            Integer menuPrice = (Integer) item.get("price");
-            //インテントオブジェクトを生成
-            Intent intent = new Intent(MainActivity.this, MenuThanksActivity.class);
-            //第2画面に送るデータを格納
-            intent.putExtra("menuName", menuName);
-            //MenuThanksActivityでのデータの受け取りと合わせるために，金額にここで「円」を追加する．
-            intent.putExtra("menuPrice", menuPrice + "円");
-            //第２画面の起動
-            startActivity(intent);
+            order(item);
         }
     }
 
@@ -127,6 +120,67 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menuListOptionCurry:
                 //カレーメニューリストデータの生成
                 _menuList = createCurryList();
+                break;
+            //それ以外
+            default:
+                //親クラスの同名メソッドを呼び出し，その戻り値をreturnValする
+                returnVal = super.onOptionsItemSelected(item);
+                break;
+        }
+        //SimpleAdapterを選択されたメニューデータで生成
+        SimpleAdapter adapter = new SimpleAdapter(MainActivity.this, _menuList, R.layout.row, FROM, TO);
+        //アダプタの登録
+        _lvMenu.setAdapter(adapter);
+        return returnVal;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, view, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context_menu_list, menu);
+        menu.setHeaderTitle(R.string.menu_list_context_header);
+    }
+
+    private void order(Map<String, Object> menu){
+        //定食名と金額を取得
+        String menuName = (String) menu.get("name");
+        Integer menuPrice = (Integer) menu.get("price");
+        //インテントオブジェクトを生成
+        Intent intent = new Intent(MainActivity.this, MenuThanksActivity.class);
+        //第2画面に送るデータを格納
+        intent.putExtra("menuName", menuName);
+        //MenuThanksActivityでのデータの受け取りと合わせるために，金額にここで「円」を追加する．
+        intent.putExtra("menuPrice", menuPrice + "円");
+        //第２画面の起動
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        //戻り値用の変数を初期値trueで用意
+        boolean returnVal = true;
+        //長押しされたビューに関するじょうほうが格納されたオブジェクトを取得
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        //長押しされたリストポジションを取得
+        int listPosition = info.position;
+        //ポジションから長押しされたメニュー情報Mapオブジェクトを取得
+        Map<String, Object> menu = _menuList.get(listPosition);
+        //選択されたメニューのIDを取得
+        int itemId = item.getItemId();
+        //IDのR値による処理の分岐
+        switch(itemId) {
+            //説明を表示メニューが選択されたときの処理
+            case R.id.menuListContextDesc:
+                //メニューの説明文字列を取得
+                String desc = (String) menu.get("desc");
+                //トーストを表示
+                Toast.makeText(MainActivity.this, desc, Toast.LENGTH_LONG).show();
+                break;
+            //ご注文メニューが押されたときの処理
+            case R.id.menuListContextOrder:
+                //ご注文処理
+                order(menu);
                 break;
             //それ以外
             default:
